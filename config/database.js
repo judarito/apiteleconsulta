f12bc05e1f7c65d6b9f42fc500980245a612dc5e
JuanDavidRicardo <juan.d.ricardo.t@gmail.com>
2020-07-16 16:29:08 -0500

    primer commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'strapi-app-db.tzkdf.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-app-db'),
+        username: env('DATABASE_USERNAME', 'admin_admin'),
+        password: env('DATABASE_PASSWORD', 'admin123456'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '246ec3da-b5e2-44e3-a1d8-8e1a1b4617de'
+};
